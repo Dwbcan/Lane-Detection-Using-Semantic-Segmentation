7882e1b886b69b6dce59b02293eb8fd12a4667ea
Dew <dew.bhaumik8@gmail.com>
2022-09-25 00:05:38 -0400

    feat: Implement lane detection on video as well as image

--- main.py
+++ main.py
@@ -50,13 +50,31 @@ def display_region_of_interest(image):
     return masked_image
 
 
-image = cv2.imread('test_image.jpg')
-lane_image = np.copy(image)
-canny_image = canny_edge_detection(lane_image)
-cropped_image = display_region_of_interest(canny_image)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-averaged_lines = average_slope_intercept(lane_image, lines)
-line_image = display_lane_lines(lane_image, averaged_lines)
-combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow('result', combo_image)
-cv2.waitKey(0)
+# Uncomment the code below to use Lane Detection algorithm on a JPEG image:
+
+# image = cv2.imread('test_image.jpg')
+# lane_image = np.copy(image)
+# canny_image = canny_edge_detection(lane_image)
+# cropped_image = display_region_of_interest(canny_image)
+# lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+# averaged_lines = average_slope_intercept(lane_image, lines)
+# line_image = display_lane_lines(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+# cv2.imshow('result', combo_image)
+# cv2.waitKey(0)
+
+
+capture = cv2.VideoCapture('test2.mp4')
+while(capture.isOpened()):
+    _, frame = capture.read()
+    canny_image = canny_edge_detection(frame)
+    isolated_image = display_region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(isolated_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lane_lines(frame, averaged_lines)
+    final_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow('result', final_image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
